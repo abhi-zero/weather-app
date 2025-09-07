13c8530dab46850c247a5f485ca8284a0fc0e68c
abhi-zero <abhi.0zero@gmail.com>
2025-09-07 14:30:18 +0530

    chore : add geocode reducer in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { weatherApi } from "../api/weatherApi";
+import { geoCodeApi } from "../api/geoCodeApi";
 
 
 export const store = configureStore({
     reducer : {
-        [weatherApi.reducerPath] : weatherApi.reducer
+        [weatherApi.reducerPath] : weatherApi.reducer,
+        [geoCodeApi.reducerPath] : geoCodeApi.reducer,
     },
-    middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(weatherApi.middleware),
+    middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(weatherApi.middleware, geoCodeApi.middleware),
 
 })
 
