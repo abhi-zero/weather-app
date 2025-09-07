b60fdf6e92001fdaa1192fdd6d26914f83a22d08
abhi-zero <abhi.0zero@gmail.com>
2025-09-07 14:50:40 +0530

    fix : add default to export

--- envVariable.js
+++ envVariable.js
@@ -1,4 +1,6 @@
-export const envVariables = {
+const envVariables = {
     weatherAPiUrl : import.meta.env.VITE_WEATHER_API_BASE_URL,
     geocode : import.meta.env.VITE_GEOCODE_API_BASE_URL
 }
+
+export default envVariables;
