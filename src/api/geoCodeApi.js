80cb065ba1dcc973e62eb1ac63523d4bbd9b4321
abhi-zero <abhi.0zero@gmail.com>
2025-09-07 14:29:46 +0530

    chore: create endpoint with rtk for geocode

--- src/api/geoCodeApi.js
+++ src/api/geoCodeApi.js
@@ -1,1 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { envVariables } from "../../envVariable";
+
+export const geoCodeApi = createApi({
+    reducerPath :'geoCodeApi',
+    baseQuery : fetchBaseQuery({baseUrl : envVariables.geocode}),
+    endpoints :(builder) =>({
+        getLocationBySearch : builder.query({
+            query : (search) => `?name=${encodeURIComponent(search)}&language=en&format=json`
+        })
+    })
+}) 
+
+export const{
+    useGetLocationBySearchQuery
+} = geoCodeApi
