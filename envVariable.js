023dafa54109958b90ca4c719bb2a0f546f5ed03
abhi-zero <abhi.0zero@gmail.com>
2025-09-07 14:09:35 +0530

    chore: add geocode api base url in env variable

--- envVariable.js
+++ envVariable.js
@@ -1,3 +1,4 @@
 export const envVariables = {
-    weatherAPiUrl : import.meta.env.VITE_WEATHER_API_BASE_URL
+    weatherAPiUrl : import.meta.env.VITE_WEATHER_API_BASE_URL,
+    geocode : import.meta.env.VITE_GEOCODE_API_BASE_URL
 }
