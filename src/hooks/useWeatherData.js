27169fe535986035d7eff8ba1105b2171ca4be63
abhi-zero <abhi.0zero@gmail.com>
2025-09-08 09:30:54 +0530

    fix : remove addiitinal logs and format document

--- src/hooks/useWeatherData.js
+++ src/hooks/useWeatherData.js
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { useEffect, useState } from 'react'
 import { useGetLocationBySearchQuery } from '../api/geoCodeApi';
 import { useGetWeatherQuery } from '../api/weatherApi';
@@ -7,41 +7,32 @@ import { useGetWeatherQuery } from '../api/weatherApi';
 export default function useWeatherData() {
   const [search, setSearch] = useState('');
   const [selectedLocation, setSelectedLocation] = useState(null);
-  const [geoLocation , setGeoLocation] = useState(null);
+  const [geoLocation, setGeoLocation] = useState(null);
 
-  useEffect(()=> {
-    if(navigator.geolocation){
+  useEffect(() => {
+    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           setGeoLocation({
-            latitude : position.coords.latitude,
-            longitude : position.coords.longitude
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
           });
         },
-        ()=> {
+        () => {
           console.warn("Geolocation not available or denied.");
         }
-        
       )
     }
-  },[])
+  }, [])
   const { data: searchData, isLoading: isSearching, error: searchError } = useGetLocationBySearchQuery(search, { skip: !search });
-   
+
   const activeLocation = selectedLocation ?? geoLocation
-  const { data: weatherData, isLoading: isWeatherLoading, error: weatherDataError } = useGetWeatherQuery( activeLocation ?? {},
-    { skip: !activeLocation})
+  const { data: weatherData, isLoading: isWeatherLoading, error: weatherDataError } = useGetWeatherQuery(activeLocation ?? {},
+    { skip: !activeLocation })
 
 
   useEffect(() => {
-    console.log(search);
-    console.log(searchData);
-
     console.log(weatherData);
-    console.log(weatherDataError);
-   
-    
-
-
   }, [search, searchData, weatherData, weatherDataError])
 
   return { setSearch, search, searchData, isSearching, searchError, weatherData, weatherDataError, isWeatherLoading, setSelectedLocation, geoLocation }
