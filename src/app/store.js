575aa3b4c438ef94f6957c3ad9c83ada3ec5c868
abhi-zero <abhi.0zero@gmail.com>
2025-09-09 23:07:33 +0530

    chore :  add units reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { weatherApi } from "../api/weatherApi";
 import { geoCodeApi } from "../api/geoCodeApi";
+import unitReducer from '../features/unitSlice'
 
 
 export const store = configureStore({
     reducer : {
         [weatherApi.reducerPath] : weatherApi.reducer,
         [geoCodeApi.reducerPath] : geoCodeApi.reducer,
+        units : unitReducer
     },
     middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(weatherApi.middleware, geoCodeApi.middleware),
 
