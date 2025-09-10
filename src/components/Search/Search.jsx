7895423980ee2727ac1eec1b776b5374f061142d
abhi-zero <abhi.0zero@gmail.com>
2025-09-10 10:08:51 +0530

    fix : make it full responsive

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { IoSearch } from "react-icons/io5";
 
 export default function Search({ setSearch, searchData,isLoading, error, setSelectedLocation}) {
 
@@ -13,18 +14,24 @@ export default function Search({ setSearch, searchData,isLoading, error, setSele
     <div>
        <div>
          <form onSubmit={handleSubmit(onSubmit)} 
-         className='flex justify-between gap-5 px-5 sm:w-[450px] md:w-[600px]'>
-          <input 
-          className='bg-neutral-800 px-6 py-3 rounded-xl w-full text-neutral-200 text-xl'
-          type="text" placeholder='Enter city name' {...register('search',{required : 'We need a value here'})}
+         className='flex md:flex-row flex-col justify-between gap-5 px-5 md:w-[600px]'>
+         <div className='relative w-full'>
+          <div className='top-[50%] left-0 absolute text-neutral-50 text-2xl -translate-y-1/2 translate-1/2'>
+            <IoSearch />
+          </div>
+           <input 
+          className='bg-neutral-800 py-3 pr-6 pl-12 rounded-xl w-full text-neutral-200 text-xl'
+          type="text" placeholder={`Enter city name`} {...register('search',{required : 'We need a value here'})}
           onChange={(e)=> setSearch(e.target.value)}/>
+
+         </div>
           <button type='submit'
-          className='bg-blue-700 hover:bg-blue-500 px-9 py-3 rounded-xl text-neutral-50 transition-all duration-300 ease-in-out cursor-pointer'
+          className='bg-blue-700 hover:bg-blue-500 px-9 py-3 rounded-xl focus:outline-2 focus:outline-blue-700 focus:outline-offset-2 text-neutral-50 transition-all duration-300 ease-in-out cursor-pointer'
           >
             Search
           </button>
         </form>
-          <p className='text-red-600 text-sm text-center'>{errors.search && errors.search.message}</p>
+          <p className='px-5 text-red-600 text-sm md:text-left text-center'>{errors.search && errors.search.message}</p>
        </div >
         {error ? 
         (<h2>error.status</h2>) 
@@ -34,7 +41,7 @@ export default function Search({ setSearch, searchData,isLoading, error, setSele
         (<h2>Loading...</h2>) 
         : 
         searchData?.results ?
-       ( <div className='z-50 bg-neutral-800 shadow-neutral-950 shadow-xl mt-3 p-2 rounded-2xl md:w-[450px] text-neutral-100'>
+       ( <div className='z-50 bg-neutral-800 shadow-neutral-950 shadow-xl mx-5 mt-3 p-2 rounded-2xl md:w-[415px] text-neutral-100'>
           <ul>
           {searchData?.results?.map((loc) => (
           <li key={`${loc.longitude}-${loc.latitude}`}
