4742b58992ba3460dafb047df14b9e761cb59258
abhi-zero <abhi.0zero@gmail.com>
2025-09-23 22:04:09 +0530

    chore: Add reverseGeoCodeApi and its reduers

--- src/app/store.js
+++ src/app/store.js
@@ -2,16 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { weatherApi } from "../api/weatherApi";
 import { geoCodeApi } from "../api/geoCodeApi";
+import { reverseGeoCodeAPi } from "../api/reverseGeoCodeApi";
 import unitReducer from '../features/unitSlice'
+import  reverseGeoCodeReducer from "../features/reverseGeocodeSlice";
 
 
 export const store = configureStore({
-    reducer : {
-        [weatherApi.reducerPath] : weatherApi.reducer,
-        [geoCodeApi.reducerPath] : geoCodeApi.reducer,
-        units : unitReducer
+    reducer: {
+        [weatherApi.reducerPath]: weatherApi.reducer,
+        [geoCodeApi.reducerPath]: geoCodeApi.reducer,
+        [reverseGeoCodeAPi.reducerPath]: reverseGeoCodeAPi.reducer,
+        units: unitReducer,
+        reverseGeoCode : reverseGeoCodeReducer,
     },
-    middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(weatherApi.middleware, geoCodeApi.middleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(weatherApi.middleware, geoCodeApi.middleware, reverseGeoCodeAPi.middleware),
 
 })
 
