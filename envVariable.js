6a20f984e87303e1da12aeb0845118ba5597113d
abhi-zero <abhi.0zero@gmail.com>
2025-09-23 22:06:53 +0530

    chore : add reverseGeocode api url and api key

--- envVariable.js
+++ envVariable.js
@@ -1,6 +1,8 @@
 const envVariables = {
     weatherAPiUrl : import.meta.env.VITE_WEATHER_API_BASE_URL,
-    geocode : import.meta.env.VITE_GEOCODE_API_BASE_URL
+    geocode : import.meta.env.VITE_GEOCODE_API_BASE_URL,
+    reverseGeocodeApiKey : import.meta.env.VITE_REVERSE_GEOCODE_API_KEY,
+    reverseGeocodeApi : import.meta.env.VITE_REVERSE_GEOCODE_API_BASE_URL,
 }
 
 export default envVariables;
