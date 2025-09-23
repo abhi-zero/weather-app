eb20e0f324ab8e8281e8ae99b72a1a9bde441261
abhi-zero <abhi.0zero@gmail.com>
2025-09-23 22:05:41 +0530

    fix : add localstorage for units

--- src/features/unitSlice.js
+++ src/features/unitSlice.js
@@ -3,19 +3,22 @@ import { createSlice } from "@reduxjs/toolkit";
 export const unitSlice = createSlice({
     name : 'units',
     initialState : {
-        tempUnit : 'celsius',
-        windUnit : 'kmh',
-        precipitationUnit : 'mm',
+        tempUnit : localStorage.getItem('tempUnit') || 'celsius',
+        windUnit : localStorage.getItem('windUnit') || 'kmh',
+        precipitationUnit : localStorage.getItem('precipitationUnit') || 'mm',
     },
     reducers : {
         selectTempUnit :(state,  action) => {
             state.tempUnit = action.payload;
+            localStorage.setItem('tempUnit', state.tempUnit)
         },
         selectWindUnit : (state,  action) => {
             state.windUnit = action.payload;
+            localStorage.setItem('windUnit', state.windUnit)
         },
         selectPrecipitationUnit :(state,  action) => {
             state.precipitationUnit = action.payload;
+            localStorage.setItem('precipitationUnit', state.precipitationUnit)
         }
     }
 })
