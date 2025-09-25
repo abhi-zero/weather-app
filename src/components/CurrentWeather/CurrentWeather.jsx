ab94353974b24726388166cd574360b7da282e80
abhi-zero <abhi.0zero@gmail.com>
2025-09-25 14:04:31 +0530

    fix: update CurrentWeather component to use props and improve loading logic

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -1,26 +1,47 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import sun from "../../assets/images/icon-sunny.webp";
-import useWeatherData from "../../hooks/useWeatherData";
+import useDate from "../../hooks/useDate/useDate";
 
-export default function CurrentWeather() {
+export default function CurrentWeather({
+  weatherData,
+  weatherDataError,
+  isWeatherLoading,
+  isLoading,
+  setLoading,
+}) {
   const location = useSelector((state) => state.reverseGeoCode);
-  const { weatherData, weatherDataError, isWeatherLoading } = useWeatherData();
 
+  // useeffect for setting loading true or false
   useEffect(() => {
     console.log(weatherData?.current?.temperature_2m);
-  }, [weatherData]);
+    if (location.name === undefined || location.country === undefined) {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+  }, [weatherData, setLoading, location.name, location.country]);
+
+  const {currentFullDate} = useDate();
+
+
   return (
     <div
-      className={`h-[286px] lg:w-[800px] max-w-[433px] md:max-w-[720px] bg-[image:var(--background-hero-mobile)] md:bg-[image:var(--background-hero-desktop)] bg-cover bg-center  rounded-3xl`}
+      className={`lg:max-w-[800px] w-full min-w-[343px]  sm:max-w-[720px]  bg-[image:var(--background-hero-mobile)] md:bg-[image:var(--background-hero-desktop)] bg-cover bg-center  rounded-3xl h-[286px]`}
     >
-      <div className="place-content-center px-[16px] md:px-[30px] h-full">
+      <div className="place-content-center px-[24px] md:px-[30px] h-full">
         <div className="flex md:flex-row flex-col justify-center-safe md:justify-between items-center-safe md:items-center-safe">
           <div className="md:text-left text-center">
             <h1 className="font-bold text-[28px] text-white">
-              <span>{location.name}</span>, <span>{location.country}</span>
+              {isLoading ? (
+                "-- --"
+              ) : (
+                <>
+                  <span>{location.name}</span>, <span>{location.country}</span>
+                </>
+              )}
             </h1>
-            <p className="font-medium text-[18px] text-white">Tuseday, Aug 5, 2025</p>
+            <p className="font-medium text-[18px] text-white">{currentFullDate}</p>
           </div>
           <div className="flex items-center">
             <div className="w-[120px] h-[120px]">
@@ -28,11 +49,14 @@ export default function CurrentWeather() {
             </div>
             <div>
               <h1 className="font-dmserif font-semibold text-[96px] text-white italic">
-                {isWeatherLoading
+                {isWeatherLoading ||
+                weatherData?.current?.temperature_2m === undefined
                   ? "-- --"
                   : weatherDataError
                   ? "Error"
-                  : `${weatherData?.current?.temperature_2m}°`}
+                  : `${weatherData?.current?.temperature_2m
+                      .toString()
+                      .split('.')[0]}\u00B0`}
               </h1>
             </div>
           </div>
