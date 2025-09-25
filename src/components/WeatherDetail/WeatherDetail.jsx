b70665162f32df19d4f6968ea839d2ad35360190
abhi-zero <abhi.0zero@gmail.com>
2025-09-25 14:05:52 +0530

    chore: add WeatherDetail component for displaying weather information

---
+++ src/components/WeatherDetail/WeatherDetail.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function WeatherDetail({ label, value ,isWeatherLoading, unitSuffix}) {
+
+  return (
+    <div>
+      <div className="flex flex-col gap-6 bg-neutral-800 p-[20px] rounded-xl md:w-[165px] lg:w-[182px] min-w-[163px] lg:h-[118px]">
+        <div>
+          <h1 className="font-medium text-[18px] text-neutral-200 leading-[120%]">{label}</h1>
+        </div>
+        <div>
+          <p className="font-light text-[32px] text-neutral-0 leading-[100%]">
+            {isWeatherLoading || value === undefined ?
+            '--'
+            :
+            <><span>{value}</span>
+            {`${unitSuffix.length > 1 ? " " : ''}`}
+             <span>{unitSuffix}</span> </>  
+        }
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
