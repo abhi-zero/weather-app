a7c488f7d7cd7177d1aea1ddc94435051a6d2c67
abhi-zero <abhi.0zero@gmail.com>
2025-09-25 14:03:51 +0530

    chore: add refetch on tab focuse and on reconnect network

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -2,14 +2,16 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import envVariables from '../../envVariable';
 
 export const weatherApi = createApi({
-    reducerPath : 'weatherApi',
-    baseQuery : fetchBaseQuery({baseUrl : envVariables.weatherAPiUrl}),
-    tagTypes : ['weather'],
-    endpoints : (builder)=> ({
-        getWeather :builder.query({
-            query : ({longitude, latitude, pUnit='mm', tUnit='celsius', wsUnit = 'kmh'}) => `?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=weather_code,temperature_2m,precipitation,relative_humidity_2m,apparent_temperature,wind_speed_10m&timezone=auto&wind_speed_unit=${wsUnit}&temperature_unit=${tUnit}&precipitation_unit=${pUnit}`
+    reducerPath: 'weatherApi',
+    baseQuery: fetchBaseQuery({ baseUrl: envVariables.weatherAPiUrl }),
+    tagTypes: ['weather'],
+    endpoints: (builder) => ({
+        getWeather: builder.query({
+            query: ({ longitude, latitude, pUnit = 'mm', tUnit = 'celsius', wsUnit = 'kmh' }) => `?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=weather_code,temperature_2m,precipitation,relative_humidity_2m,apparent_temperature,wind_speed_10m&timezone=auto&wind_speed_unit=${wsUnit}&temperature_unit=${tUnit}&precipitation_unit=${pUnit}`
         }),
-    })
+    }),
+    refetchOnFocus: true,
+    refetchOnReconnect: true,
 })
 
 
