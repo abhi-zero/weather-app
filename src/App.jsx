b7a0b9e293b650269e5b4dfda0a81122272913f2
abhi-zero <abhi.0zero@gmail.com>
2025-09-25 14:06:54 +0530

    fix: clean up App component by removing unused state and improving layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
-import { useState } from 'react'
+import useWeatherData from "./hooks/useWeatherData";
+import WeatherLayout from "./components/WeatherLayout";
+import Search from "./components/Search/Search";
+import NavBar from "./components/Header/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    setSearch,
+    search,
+    searchData,
+    isSearching,
+    searchError,
+    setSelectedLocation,
+  } = useWeatherData();
 
   return (
-    <div className='flex flex-col justify-center-safe items-center-safe'>
+    <div className="flex flex-col items-center-safe bg-neutral-900 w-[100vw] min-h-screen">
+      <NavBar />
       <div>
+        <div className="my-[30px]">
+          <h1 className="px-[20px] font-bg text-[52px] text-neutral-50 text-center text-wrap leading-14">
+            How's the sky looking today?
+          </h1>
+        </div>
+        <div className="mx-auto md:w-fit">
+          <Search
+            searchData={searchData}
+            isLoading={isSearching}
+            error={searchError}
+            setSelectedLocation={setSelectedLocation}
+            setSearch={setSearch}
+            search={search}
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="my-8 lg:my-12 w-full">
+        <WeatherLayout />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
