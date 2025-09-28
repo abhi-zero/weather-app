d501bba0cf1999ad15869fd44c9e1d9a18153068
abhi-zero <abhi.0zero@gmail.com>
2025-09-28 12:47:57 +0530

    refactor: remove unused useEffect import in HourlyWeather component

--- src/components/HourlyWeather/HourlyWeather.jsx
+++ src/components/HourlyWeather/HourlyWeather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import DropDownButton from "../DropdownButton/DropDownButton";
 import { IoIosArrowDown } from "react-icons/io";
 import useDate from "../../hooks/useDate/useDate";
